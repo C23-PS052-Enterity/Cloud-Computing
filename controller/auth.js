857c0de6f5861b4c1556d56922b2d45a21f8904a
alibarmonster <alibar838@gmail.com>
2023-05-25 11:11:46 +0700

    login success created

--- controller/auth.js
+++ controller/auth.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 const { hashPassword, comparePassword } = require('../helpers/bcrypt');
 const jwt = require('jsonwebtoken');
 const { v4: uuid } = require('uuid');
-const { validateRegistration } = require('../validator/auth-joiSchema');
+const { validateRegistration, validateLogin } = require('../validator/auth-joiSchema');
 
 const register = async (req, res) => {
   try {
@@ -33,7 +33,7 @@ const register = async (req, res) => {
       nama_toko: nama_toko,
       email: email,
       password: encryptedPassword,
-      profile_picture: null,
+      profile_picture: 'https://storage.googleapis.com/enterity/enterity.png',
     });
 
     return res.status(201).json({
@@ -49,4 +49,51 @@ const register = async (req, res) => {
   }
 };
 
-module.exports = { register };
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const { error } = validateLogin.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        message: error.message,
+      });
+    }
+
+    const retrievedUser = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!retrievedUser) {
+      return res.status(400).json({
+        message: 'Email is not registered',
+      });
+    }
+
+    const passwordMatches = await comparePassword(password, retrievedUser.password);
+
+    if (passwordMatches) {
+      const jwtToken = {
+        email: retrievedUser.email,
+        payload: retrievedUser.payload,
+      };
+
+      const token = jwt.sign(jwtToken, process.env.ACCESS_TOKEN_KEY);
+
+      return res.status(200).json({
+        status: 'success',
+        message: 'user logged in Succesfully',
+        token: token,
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 'failed',
+      message: err.message,
+    });
+  }
+};
+
+module.exports = { register, login };
--- validator/auth-joiSchema.js
+++ validator/auth-joiSchema.js
@@ -17,4 +17,18 @@ const validateRegistration = Joi.object({
   }),
 });
 
-module.exports = { validateRegistration };
+const validateLogin = Joi.object({
+  email: Joi.string()
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+    .required()
+    .messages({
+      'string.empty': 'email tidak boleh kosong',
+      'string.email': 'email harus valid',
+    }),
+  password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,30}$')).required().messages({
+    'string.pattern.base':
+      'password harus terdiri dari 8 sampai 30 karakter dan hanya mengandung huruf dan angka ',
+  }),
+});
+
+module.exports = { validateRegistration, validateLogin };
