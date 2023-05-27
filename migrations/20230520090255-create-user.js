7174985da36109682d459ae47572a420fe453de7
alibarmonster <alibar838@gmail.com>
2023-05-28 06:52:00 +0700

    change id on migration

--- migrations/20230520090255-create-user.js
+++ migrations/20230520090255-create-user.js
@@ -6,8 +6,7 @@ module.exports = {
       id: {
         allowNull: false,
         primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.STRING,
       },
       nama_toko: {
         type: Sequelize.STRING,
