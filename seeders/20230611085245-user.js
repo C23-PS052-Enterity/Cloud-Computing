b89caf682914e2e00c4e5198ecb007c222db86a6
alibarmonster <alibar838@gmail.com>
2023-06-11 15:57:03 +0700

    add user to be seeded

---
+++ seeders/20230611085245-user.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const { query } = require('express');
+const user = require('../data/user.json');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    const dataUserToBeSeeded = user.map((eachUser) => {
+      return {
+        id: eachUser.id,
+        nama_toko: eachUser.nama_toko,
+        email: eachUser.email,
+        password: eachUser.password,
+        profile_picture: eachUser.profile_picture,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+    });
+
+    await queryInterface.bulkInsert('Users', dataUserToBeSeeded, {});
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
